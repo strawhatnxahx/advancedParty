40c0217b192a7ee20e22933d1a1cd019db92fb26
Noah Haberkorn <noah.haberkorn@student.kdg.be>
2022-02-15 10:45:50 +0100

    first commit

---
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -0,0 +1,22 @@
+package be.thomasmore.party.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping({"/", "/home"})
+    public String home(Model model) {
+        int myCalculatedValue = 34 * 62;
+        model.addAttribute("myCalculatedValue", myCalculatedValue);
+        return "home";
+    }
+
+    public String about(Model model) {
+        int myCalculatedValue = 34 * 62;
+        model.addAttribute("myCalculatedValue", myCalculatedValue);
+        return "home";
+    }
+
+}
