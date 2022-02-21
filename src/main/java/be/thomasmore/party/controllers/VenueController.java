494928b5da97547e861dd3091f9b3f5ff9876661
Noah Haberkorn <noah.haberkorn@student.kdg.be>
2022-02-21 10:04:51 +0100

    Zipping the file messed up the project. New commit.

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -1,31 +1,32 @@
 package be.thomasmore.party.controllers;
 
 import be.thomasmore.party.model.Venue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class VenueController {
+    @Autowired
+    private be.thomasmore.party.repos.VenueRepository venueRepository;
+
 
     @GetMapping("/venuedetails")
+
+
     public String venueDetails(Model model) {
-        Venue venue = new Venue("VenueZonderNaam","www.venuezondernaam.org");
-        model.addAttribute("venue",venue);
+    ///    Venue venue = new Venue("VenueZonderNaam","www.venuezondernaam.org");
+    ///    model.addAttribute("venue",venue);
 
-        int capacity = 100;
-        boolean foodProvided = true;
-        boolean indoor = true;
-        boolean outdoor = false;
-        boolean freeParking = false;
-        double distance = 1.0;
-        String city = "Antwerpen";
-
-
-
-
+        Optional<Venue> optionalVenue = venueRepository.findById(1);
+        if (optionalVenue.isPresent()) {
+            model.addAttribute("venue", optionalVenue.get());
+        }
         return "venuedetails";
     }
+
 }
